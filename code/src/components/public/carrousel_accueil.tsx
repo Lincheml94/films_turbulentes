77ed9e4fdac303caa67b4afadb40569e0ed773fc
Lincheml94 <lina.chemlal@gmail.com>
2026-09-22 13:24:06 +0200

    maj

--- code/src/components/public/carrousel_accueil.tsx
+++ code/src/components/public/carrousel_accueil.tsx
@@ -1,8 +1,22 @@
+import styles from "../../assets/css/public/carrousel.module.css";
+
 const CarrouselAccueil = () => {
 	return (
-		<div>
-			<div></div>
-			<div></div>
+		<div className={styles.mainCarrousel}>
+			<div className={styles.legend}>
+				<div className={styles.texte}>
+					<p className={styles.legendTitle}>Les Saintes</p>
+					<p className={styles.legendText}>2025</p>
+					<p className={styles.legendText}>Un film de Melissandre Carrasco</p>
+				</div>
+				<div className={styles.fleches}>
+					<img src="/img/icons/arrow_right.svg" alt="" />
+					<img src="/img/icons/arrow_left.svg" alt="" />
+				</div>
+			</div>
+			<div className={styles.poster}>
+				<img src="/img/affiches/les_saintes.jpg" alt="Films Les Saintes" />
+			</div>
 		</div>
 	);
 };
--- code/src/pages/index.tsx
+++ code/src/pages/index.tsx
@@ -1,23 +1,11 @@
-import styles from "../assets/css/public/trait.module.css";
 import Header from "../components/header";
+import CarrouselAccueil from "../components/public/carrousel_accueil";
 
 const HomePage = () => {
 	return (
 		<>
 			<Header variant="home" />
-			{/* <div className={styles.trait}></div> */}
-			<div className={styles.infoTrait}>
-				<div className={styles.infoTexte}>
-					<p>
-						Créée à l’été 2023 à Guainville en région Centre Val de Loire, Les
-						Films des Turbulentes défend un cinéma de l’exploration qui
-						bouleverse autant qu’il bouscule. Nous accompagnons des cinéastes de
-						la nouvelle garde, soucieux du monde qui les entoure et dont les
-						récits résonnent avec leur vécu intime.
-					</p>
-				</div>
-				<div className={styles.trait}></div>
-			</div>
+			<CarrouselAccueil />
 		</>
 	);
 };
